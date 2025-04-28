9ed8d01ba6b2d1b92fa028a7282cba38d3fa9638
SWARNIM <swarnimchandve23@gmail.com>
2025-04-28 08:21:22 +0000

    minor-changes3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import { Analytics } from "@vercel/analytics/react"
 import ConvexClerkProvider from "@/providers/ConvexClerkProvider";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
@@ -44,6 +44,7 @@ export default function RootLayout({
         <main className="pt-24  flex-grow">
 
           {children}
+          <Analytics />
         </main>
         <Footer/>
       </body>
